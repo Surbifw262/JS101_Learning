05c6bbec883a8ae92b2c6b8670b280927da56438
Surabhi <surabhi24jrk@gmail.com>
2023-01-02 22:49:30 +0530

    firstday

--- ftweb25-dayfirstass (2)/problem3.js
+++
@@ -1,6 +0,0 @@
-//Create a variable name to store your name
-//Create a variable age to store your age
-//Print the values stored in the variable on one line followed by the type of the variable in the next line//
-let a="Name = Surabhi,";
-let b="age = 26,";
-console.log(a ,"\n",b);
